7ab3d615b7f6c54fbeacabbf3f2b45269cb595eb
Julli Mayanne <jullimayanne9@gmail.com>
2023-08-08 21:35:42 -0300

    feat: create a new branch

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-const express = require('express');
+const express = require("express");
 
 const server = express();
-const filmes = require('./src/data/filmes.json')
+const filmes = require("./src/data/filmes.json");
+const cors = require("cors");
 
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 
-server.get('/filmes',(req, res) => {
-    return res.json(filmes)
+server.use(cors(corsOptions));
+server.get("/filmes", (req, res) => {
+  return res.json(filmes);
 });
 
 server.listen(8080, () => {
-  console.log('servidor está funcionando...')
+  console.log("servidor está funcionando...");
 });
